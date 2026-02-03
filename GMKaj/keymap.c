104789d37714db380d4b28964bb4d373a1c77ded
Dave <davids@mayerglobal.com>
2026-02-03 16:30:17 -0500

    changed paste to 2 functions instead of 3

--- GMKaj/keymap.c
+++ GMKaj/keymap.c
@@ -12,12 +12,10 @@ enum custom_keycodes {
   HSV_41_255_255,
   ST_MACRO_0,
   ST_MACRO_1,
+  DF_WSE_WSV,
 };
 
 
-enum tap_dance_codes {
-  DANCE_0,
-};
 
 #define DUAL_FUNC_0 LT(8, KC_Y)
 #define DUAL_FUNC_1 LT(1, KC_R)
@@ -32,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     DUAL_FUNC_0,    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           DUAL_FUNC_1,                                    LALT(KC_SPACE), KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     KC_DELETE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(1),                                          OSL(1),         KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
     KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           DUAL_FUNC_3,    MT(MOD_LGUI, KC_QUOTE),
-    DUAL_FUNC_2,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           TD(DANCE_0),                                    OSL(3),         KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       DUAL_FUNC_4,
+    DUAL_FUNC_2,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           DF_WSE_WSV,                                    OSL(3),         KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       DUAL_FUNC_4,
     KC_GRAVE,       KC_QUOTE,       CW_TOGG,        KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_LBRC,        KC_RBRC,        TO(2),
                                                                                                     KC_LEFT_CTRL,   KC_RIGHT_ALT,   KC_RIGHT_ALT,   KC_RIGHT_CTRL,
                                                                                                                     KC_HOME,        LCTL(KC_A),
@@ -69,9 +67,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
-
-
-
 
 
 
@@ -133,74 +128,22 @@ bool rgb_matrix_indicators_user(void) {
 
 
 
-typedef struct {
-    bool is_press_action;
-    uint8_t step;
-} tap;
-
-enum {
-    SINGLE_TAP = 1,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    DOUBLE_SINGLE_TAP,
-    MORE_TAPS
-};
-
-static tap dance_state[1];
-
-uint8_t dance_step(tap_dance_state_t *state);
-
-uint8_t dance_step(tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return SINGLE_TAP;
-        else return SINGLE_HOLD;
-    } else if (state->count == 2) {
-        if (state->interrupted) return DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return DOUBLE_HOLD;
-        else return DOUBLE_TAP;
-    }
-    return MORE_TAPS;
-}
-
-
-void on_dance_0(tap_dance_state_t *state, void *user_data);
-void dance_0_finished(tap_dance_state_t *state, void *user_data);
-void dance_0_reset(tap_dance_state_t *state, void *user_data);
-
-void on_dance_0(tap_dance_state_t *state, void *user_data) {
-    if(state->count == 3) {
-        tap_code16(LCTL(KC_V));
-        tap_code16(LCTL(KC_V));
-        tap_code16(LCTL(KC_V));
-    }
-    if(state->count > 3) {
-        tap_code16(LCTL(KC_V));
-    }
-}
-
-void dance_0_finished(tap_dance_state_t *state, void *user_data) {
-    dance_state[0].step = dance_step(state);
-    switch (dance_state[0].step) {
-        case SINGLE_TAP: tap_code16(LGUI(LSFT(KC_E))); break;
-        case SINGLE_HOLD: tap_code16(LGUI(LSFT(KC_V))); break;
-        case DOUBLE_TAP: tap_code16(LCTL(KC_V)); break;
-        case DOUBLE_HOLD: tap_code16(LCTL(KC_V)); break;
-        case DOUBLE_SINGLE_TAP: tap_code16(LCTL(KC_V)); tap_code16(LCTL(KC_V)); break;
-    }
-}
-
-void dance_0_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(10);
-    dance_state[0].step = 0;
-}
-
-tap_dance_action_t tap_dance_actions[] = {
-        [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
-};
+static uint16_t df_wse_wsv_timer = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+  case DF_WSE_WSV:
+      if (record->event.pressed) {
+        df_wse_wsv_timer = timer_read();
+      } else {
+        if (timer_elapsed(df_wse_wsv_timer) < TAPPING_TERM) {
+          tap_code16(LGUI(LSFT(KC_E)));
+        } else {
+          tap_code16(LGUI(LSFT(KC_V)));
+        }
+      }
+      return false;
+
   case QK_MODS ... QK_MODS_MAX: 
     // Mouse keys with modifiers work inconsistently across operating systems, this makes sure that modifiers are always
     // applied to the mouse key that was pressed.
