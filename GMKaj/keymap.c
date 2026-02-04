e2ff27cdd42631ee85859447dd7c1d868e9c5694
Dave <davids@mayerglobal.com>
2026-02-04 11:33:38 -0500

    temp remove win shift E

--- GMKaj/keymap.c
+++ GMKaj/keymap.c
@@ -181,7 +181,8 @@ void dance_0_finished(tap_dance_state_t *state, void *user_data) {
     switch (dance_state[0].step) {
         case SINGLE_TAP: register_code16(LCTL(KC_V)); break;
         case SINGLE_HOLD: register_code16(LGUI(LSFT(KC_V))); break;
-        case DOUBLE_TAP: register_code16(RGUI(RSFT(KC_E))); break;
+        case DOUBLE_TAP: register_code16(LGUI(LSFT(KC_V))); break;
+      // case DOUBLE_TAP: register_code16(RGUI(RSFT(KC_E))); break;
         case DOUBLE_SINGLE_TAP: tap_code16(LCTL(KC_V)); register_code16(LCTL(KC_V));
     }
 }
