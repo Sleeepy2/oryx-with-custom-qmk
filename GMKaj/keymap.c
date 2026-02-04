c66421f249fafc5a690c9866e97b6b70b0c5b1a0
Dave <davids@mayerglobal.com>
2026-02-04 15:19:29 -0500

    another try for double tap

--- GMKaj/keymap.c
+++ GMKaj/keymap.c
@@ -180,9 +180,8 @@ void dance_0_finished(tap_dance_state_t *state, void *user_data) {
     dance_state[0].step = dance_step(state);
     switch (dance_state[0].step) {
         case SINGLE_TAP: register_code16(LCTL(KC_V)); break;
-        case SINGLE_HOLD: register_code16(LGUI(LSFT(KC_V))); break;
-        case DOUBLE_TAP: register_code16(LGUI(LSFT(KC_V))); break;
-      // case DOUBLE_TAP: register_code16(RGUI(RSFT(KC_E))); break;
+        case SINGLE_HOLD: register_code16(LGUI(KC_V)); break;
+        case DOUBLE_TAP: register_code16(LSG(KC_E)); break;
         case DOUBLE_SINGLE_TAP: tap_code16(LCTL(KC_V)); register_code16(LCTL(KC_V));
     }
 }
